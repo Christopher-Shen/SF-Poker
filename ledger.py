448cc339d65b307936cdd2a46aee1fdb767764de
Christopher-Shen <chris.y.shen@gmail.com>
2020-05-31 23:40:57 -0400

    Add ledger calculations

--- ledger.py
+++ ledger.py
@@ -1,16 +1,121 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+import argparse
+import gspread  # type: ignore
+import json
+import pandas as pd  # type: ignore
 
-scope: list = [
-    "https://spreadsheets.google.com/feeds",
-    "https://www.googleapis.com/auth/drive",
-]
-creds: ServiceAccountCredentials = ServiceAccountCredentials.from_json_keyfile_name(
-    "SF_Poker_Secrets.json", scope
-)
-client = gspread.authorize(creds)
+from heapq import heapify, heappush, heappop
+from oauth2client.service_account import ServiceAccountCredentials  # type: ignore
+from typing import List, Tuple, Dict, Any, Union, Set
 
-sheet: dict = client.open("SF Poker").sheet1
 
-test_sheet = sheet.get_all_records()
-print(test_sheet)
+def compute_transactions(ledger):
+    assert round(sum(ledger.values()), 2) == 0
+    neg = []
+    pos = []
+    for name, value in ledger.items():
+        if value < 0:
+            heappush(neg, (value, value, name))
+        else:
+            heappush(pos, (-value, value, name))
+    transactions = []
+    while neg and pos:
+        _, debt, debtee = heappop(pos)
+        __, payment, debtor = heappop(neg)
+        unaccounted = round(debt + payment, 2)
+        if unaccounted > 0:
+            heappush(pos, (-unaccounted, unaccounted, debtee))
+        elif unaccounted < 0:
+            heappush(neg, (unaccounted, unaccounted, debtor))
+        amount = min(debt, -payment)
+        transactions.append((debtee, debtor, amount))
+    assert len(neg) == 0
+    assert len(pos) == 0
+    transactions = sorted(transactions)
+
+    return transactions
+
+
+def get_spreadsheet_data() -> Tuple[Dict[str, float], Dict[str, str]]:
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive",
+    ]
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
+        "SF_Poker_Secrets.json", scope
+    )
+    client = gspread.authorize(creds)
+
+    google_sheet_client = client.open("SF Poker")
+
+    results_worksheet = google_sheet_client.worksheet("result").get_all_records()
+
+    cleaned_results_worksheet = [
+        row for row in results_worksheet if row.get("venmo_handle").startswith("@")
+    ]
+
+    list_of_results_lists = google_sheet_client.worksheet("result").get_all_values()
+
+    game_ids_to_settle: List[str] = [
+        list_of_results_lists[0][i + 2]
+        for i, is_done in enumerate(list_of_results_lists[4][2:])
+        if not is_done
+    ]
+
+    length_ids = len(game_ids_to_settle)
+    game_ids_to_settle_set = set(game_ids_to_settle)
+
+    assert len(game_ids_to_settle_set) == length_ids
+
+    all_results = {}
+    venmo_info: Dict[str, str] = {}
+
+    for row in cleaned_results_worksheet:
+        player_results = {}
+        for gid in game_ids_to_settle:
+            if gid in row:
+                player_pnl = row[gid]
+                if player_pnl:
+                    player_results[gid] = player_pnl
+        player_name = row["name"]
+        player_venmo = row["venmo_handle"]
+
+        assert player_name not in player_results
+        if player_results:
+            all_results[player_name] = player_results
+            venmo_info[player_name] = player_venmo
+
+    summed_results: Dict[str, float] = {
+        name: round(sum(all_results[name].values()), 2)
+        for (name, results_dict) in all_results.items()
+    }
+
+    return summed_results, venmo_info
+
+
+def print_ledger(data, venmo_info, transactions) -> str:
+    out_string = "BILLS\n=============\n"
+
+    for name in data:
+        if data[name] < 0:
+            out_string += f"{name}: -${-data[name]}\n"
+        else:
+            out_string += f"{name}: ${data[name]}\n"
+
+    out_string += "\nTransactions To Settle\n======================\n"
+    for debtee, debtor, amount in transactions:
+        out_string += f"{debtee} ({venmo_info.get(debtee)}) requests ${amount} from {debtor} ({(venmo_info.get(debtor))})\n"
+
+    return out_string
+
+
+def main():
+    data, venmo_info = get_spreadsheet_data()
+
+    transactions = compute_transactions(data)
+
+    out_string = print_ledger(data, venmo_info, transactions)
+    print(out_string)
+
+
+if __name__ == "__main__":
+    main()
